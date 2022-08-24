9f05b8c246f403d9cc8b5136588e8ded336ad492
Dmytro Syvenko <syvenkod@gmail.com>
2022-08-24 15:17:06 +0300

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    SidebarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
