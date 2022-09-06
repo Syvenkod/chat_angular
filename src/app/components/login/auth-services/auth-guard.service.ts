82e8dd9b39969542c1bcd0552efb5449b57550ca
Dmytro Syvenko <syvenkod@gmail.com>
2022-09-06 20:49:37 +0300

    some refactoring

--- src/app/components/login/auth-services/auth-guard.service.ts
+++ src/app/components/login/auth-services/auth-guard.service.ts
@@ -5,13 +5,8 @@ import { CanActivateChild, CanActivate, ActivatedRouteSnapshot, RouterStateSnaps
 @Injectable({
   providedIn: 'root'
 })
-// export class AuthGuardService implements CanActivate {
 export class AuthGuardService implements CanActivate {
   constructor(private authService: AuthService) { }
-  // методы canActivate и canActivateChild возращают
-  // Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
-  // (некоторые поправки возвращаемого типа зависят от версии Ангуляр)
-  // Возвращаемое значение определяет будет ли активирован маршрут
 
   canActivate() {
       return this.authService.isLoggedIn()
