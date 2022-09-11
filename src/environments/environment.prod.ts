a6394231b7c2d5430bad49cb08460b4be6cb07e0
Dmytro Syvenko <syvenkod@gmail.com>
2022-09-11 22:49:41 +0300

    another fix)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCY09Nh-RAUvbBcjq6--GCjcoWF_IAYO7Y",
+    authDomain: "chat-angular-5ccb9.firebaseapp.com",
+    projectId: "chat-angular-5ccb9",
+    storageBucket: "chat-angular-5ccb9.appspot.com",
+    messagingSenderId: "1089322634676",
+    appId: "1:1089322634676:web:089b5166d6cefe4d1bf6e1"
+  },
 };
